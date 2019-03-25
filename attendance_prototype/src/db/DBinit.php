766b87bdf60b63e02b1c9e146b4e041a04dd2832
PFlorians <patrikflorians@gmail.com>
2019-03-25 16:35:52 +0100

    Update DBinit.php

--- attendance_prototype/src/db/DBinit.php
+++ attendance_prototype/src/db/DBinit.php
@@ -9,69 +9,30 @@
         private $bonusMapper;
         private $absenceMapper;
         private $summaryMapper;
+        private $dbRequestHandler;
         function __construct($dbc)
         {
             $this->dbconn = $dbc;
         }
         //this function intializes all mappers -> who will fetch data using stored procedures, so the front-end objects can
         //parse this and insert it into the view
-        public function mapperInitializer($uname, $monthAtt)
+        public function mapperInitializer()
         {
             //instantiation
             $this->basicMapper=new \attendance\SummaryBasicMapper($this->dbconn);
             $this->bonusMaper=new \attendance\BonusMapper($this->dbconn);
             $this->absenceMapper=new \attendance\AbsenceMapper($this->dbconn);
             $this->summaryMapper=new \attendance\MonthlySummaryMapper($this->dbconn);
-
+            $this->dbRequestHandler=new \attendance\DBRequestHandler($this->dbconn);//initiator is called elsewhere
+        }
+        public function map($uname, $monthAtt)//maps data according to given username and month
+        {
             $this->basicMapper->getMonthlyAttendanceOfUser($uname, $monthAtt);
             $this->bonusMaper->getMonthlyBonusesOfUser($uname, $monthAtt);
             $this->absenceMapper->getMonthlyAbsencesOfUser($uname, $monthAtt);
             $this->summaryMapper->getMonthlySummaryOfUser($uname, $monthAtt);
         }
-        public function getMonthlyAttendanceOfUser($uname, $monthAtt)
-        {
-            $sqlQryRes="";//error variable
-            $paramsArray=array(
-                array(&$uname, SQLSRV_PARAM_IN),
-                array(&$monthAtt, SQLSRV_PARAM_IN),
-                array(&$sqlQryRes, SQLSRV_PARAM_OUT)
-            );
-            $res=[];
-            $qry = "exec getMonthlyAttendanceOfUser @ulogin = ?, @monthAtt=?, @errMsg=?";
-            $stmt = sqlsrv_prepare($this->dbconn, $qry, $paramsArray);
-            if($stmt===false)//error on preparation
-            {
-                die(print_r(sqlsrv_errors(), true));
-            }
-            else
-            {
-                if(sqlsrv_execute($stmt))
-                {
-                    while($row=sqlsrv_fetch_object($stmt))
-                    {
-                        //echo $row->day ." ".$row->from." ".$row->until." ".$row->hours_worked_day."<br/>";
-                        $res[]=array($row->day, $row->from, $row->until, $row->hours_worked_day, $row->shifttype);
-                    }
-                    while(sqlsrv_next_result($stmt))
-                    {
-                        ;
-                    }//needs to be called after fetch
-                    if($sqlQryRes!="")
-                    {
-                        die(print_r($sqlQryRes, true));
-                    }
-                    else
-                    {
-                        var_dump($res);
-                        return $res;
-                    }
-                }
-                else
-                {
-                    die(print_r(sqlsrv_errors(), true));
-                }
-            }
-        }
+        //getters and setters go here
         public function getBasicMapper()
         {
             return $this->basicMapper;
@@ -87,6 +48,10 @@
         public function getSummaryMapper()
         {
             return $this->summaryMapper;
+        }
+        public function getDBRequestHandler()
+        {
+            return $this->dbRequestHandler;
         }
     }
  ?>
